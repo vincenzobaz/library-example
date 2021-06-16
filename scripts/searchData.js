43f52b54b76d4e683e8c9735320417e8a10a766f
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2021-06-16 07:32:13 +0000

    Deploying to gh-pages from @ vincenzobaz/library-example@28ff32d1b57f773c7daa56f94305db7a2396c135 🚀

---
+++ scripts/searchData.js
@@ -0,0 +1,9 @@
+pages = [{"l":"docs/index.html","n":"library-example","t":"library-example","d":""},
+{"l":"docs/contributing.html","n":"contributing","t":"contributing","d":""},
+{"l":"docs/getting-started.html","n":"getting-started","t":"getting-started","d":""},
+{"l":"docs/reference.html","n":"reference","t":"reference","d":""},
+{"l":"index.html","n":"index","t":"index","d":""},
+{"l":"api/index.html","n":"API","t":" API","d":"api"},
+{"l":"api/ch/epfl/scala.html","n":"ch.epfl.scala","t":"package ch.epfl.scala","d":"ch/epfl/scala"},
+{"l":"api/ch/epfl/scala/Example$.html","n":"Example","t":"object Example","d":"ch/epfl/scala/Example$"},
+{"l":"api/ch/epfl/scala/Example$.html","n":"doNothing","t":"def doNothing[A](a: A): A","d":"ch/epfl/scala/Example$"}];
