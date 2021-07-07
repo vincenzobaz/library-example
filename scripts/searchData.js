d3c35467db5688b55e89be6ec6ae381b9d93231a
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2021-07-07 12:46:52 +0000

    Deploying to gh-pages from @ vincenzobaz/library-example@ae0af10f571cedae9ec537282db6d00f5f5f2750 🚀

--- scripts/searchData.js
+++ scripts/searchData.js
@@ -1,4 +1,10 @@
-pages = [{"l":"api/index.html","n":"API","t":" API","d":"api"},
+pages = [{"l":"docs/index.html","n":"library-example","t":"library-example","d":""},
+{"l":"docs/getting-started.html","n":"Getting started","t":"Getting started","d":""},
+{"l":"docs/reference.html","n":"Reference","t":"Reference","d":""},
+{"l":"docs/contributing/index.html","n":"contributing","t":"contributing","d":""},
+{"l":"docs/contributing/releasing.html","n":"Realeasing","t":"Realeasing","d":""},
+{"l":"docs/contributing/setup.html","n":"Setup","t":"Setup","d":""},
+{"l":"api/index.html","n":"API","t":" API","d":"api"},
 {"l":"api/ch/epfl/scala.html","n":"ch.epfl.scala","t":"package ch.epfl.scala","d":"ch/epfl/scala"},
 {"l":"api/ch/epfl/scala/Example$.html","n":"Example","t":"object Example","d":"ch/epfl/scala/Example$"},
 {"l":"api/ch/epfl/scala/Example$.html","n":"doNothing","t":"def doNothing[A](a: A): A","d":"ch/epfl/scala/Example$"}];
